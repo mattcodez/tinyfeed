6e3f5ea030623dbbc6ae45ae451713bceee91612
therealbrandon <brandonmestep@gmail.com>
2014-10-08 00:34:10 -0400

    Layout changes 6

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$('#videoFeed').css('max-height', ($(window).height() - 35) + 'px');
+	$('#videoFeed').css('max-height', ($(window).height() - 42) + 'px');
 
 	$(window).on('resize', function() {
-		$('#videoFeed').css('max-height', ($(window).height() - 35) + 'px');
+		$('#videoFeed').css('max-height', ($(window).height() - 42) + 'px');
 	});
 });
